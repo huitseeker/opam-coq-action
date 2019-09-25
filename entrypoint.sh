652c54c559a86e66082bbd67d57d682c538d0f3d
François Garillot <francois@garillot.net>
2019-09-24 22:54:21 -0700

    add an opam update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@
 if [ -d "${GITHUB_WORKSPACE}/.opam" ]; then
   mv /home/opam/.opam /home/opam/.opam.old
 else
-  sudo chown -R opam ${GITHUB_WORKSPACE}
-  mv /home/opam/.opam ${GITHUB_WORKSPACE}/.opam
+  sudo chown -R opam "${GITHUB_WORKSPACE}"
+  mv /home/opam/.opam "${GITHUB_WORKSPACE}/.opam"
 fi
-ln -s ${GITHUB_WORKSPACE}/.opam /home/opam/.opam
+ln -s "${GITHUB_WORKSPACE}/.opam" /home/opam/.opam
 
 if [ "${OCAML_VERSION}" != "" ]; then
   OPAMSWITCH="${OCAML_VERSION}"
@@ -16,9 +16,9 @@ fi
 git -C /home/opam/opam-repository pull && opam update -uy
 
 if [ "$*" = "" ]; then
-  opam repo add coq-released https://coq.inria.fr/opam/released
-  opam pin add -n .
-  opam install -y coq-mathcomp-ssreflect coq-fcsl-pcm
+    opam repo add coq-released https://coq.inria.fr/opam/released
+    opam update
+    opam install -y coq-mathcomp-ssreflect coq-fcsl-pcm
 else
-  sh -c "opam $*"
+    sh -c "opam $*"
 fi
